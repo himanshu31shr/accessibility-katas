a66964ec72317b7006b239fb41e265d9d01cdae2
Himanshu Shrivastava <hshrivastava@kyruus.com>
2025-01-27 18:45:50 +0530

    added test framework for react

--- src/components/AccessibleAccordian/test/AccessibleAccordian.test.tsx
+++ src/components/AccessibleAccordian/test/AccessibleAccordian.test.tsx
@@ -1,6 +1,15 @@
 import { describe, expect, it } from "vitest";
+import { axe } from "vitest-axe";
+import AccessibleAccordian from "../AccessibleAccordian";
+import { render } from '@testing-library/react'
 
 describe("Accessible Accordian", () => {
+  it("passed accessibility", async () => {
+    const { container } = render(<AccessibleAccordian />);
+    const co = await axe(container);
+    expect(co.violations.length).toBe(0);
+  });
+
   it("passes this test", () => {
     expect(0).toBe(1);
   });
